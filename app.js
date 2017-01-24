cde6e43117ed84ed25d9b7acb65c485356865ba7
Humbert <moreaux.humbert@gmail.com>
2017-01-24 13:30:34 +0100

    Base de l'application avec les 3 vues * prof * élève * retroprojecteur

--- app.js
+++ app.js
@@ -6,28 +6,23 @@ var server  = express();
 // Page d'accueil
 server.get( '/', function ( req, res ) {
 	res.setHeader( 'Content-Type', "text/html; charset=utf-8" );
-	res.end( '<html>' +
-		 '<head>' +
-		 '<style>' +
-		 'p {color : blue;}' +
-		 '</style>' +
-		 '</head>' +
-		 '<body>' +
-		 '<p>Vous êtes sur la page d\'accueil</p>' +
-		 '</body>' +
-		 '</html>' );
+    params = [];
+    res.end('<html><body>Vous êtes sur la page d\'accueil</body></html>');
+	// res.render('/views/manager.ejs', params);
 } )
 
-// Page de détails (e.g. liste des objets)
-	.get( '/detail.html', function ( req, res ) {
-		res.setHeader( 'Content-Type', "text/plain; charset=utf-8" );
-		res.end( 'Vous êtes sur une page de détail' );
+    // Page de détails (e.g. liste des objets)
+	.get( '/manager.html', function ( req, res ) {
+		res.render('manager.ejs', []);
 	} )
+
+    .get( '/:idQuestion/question.html', function ( req, res ) {
+        res.render('question.ejs', req.params);
+    } )
 	
 	// Page de détails d'un objet
-	.get( '/pages/:idObjet/detail.html', function ( req, res ) {
-		res.setHeader( 'Content-Type', 'text/plain; charset=utf-8' );
-		res.end( 'Vous êtes sur la page de détail pour l\'objet ' + req.params.idObjet );
+	.get( '/:idQuestion/resultat.html', function ( req, res ) {
+        res.render('resultat.ejs', req.params);
 	} )
 	
 	.use( function ( req, res, next ) {
