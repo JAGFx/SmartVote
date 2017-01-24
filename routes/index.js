6609e5326f0da07b3198151fdcb2eaa6c96bb52d
JAGFx <contact@jagfx.fr>
2017-01-24 14:59:33 +0100

    Feature: Pass question to EJS view

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,48 @@
-var express = require('express');
-var router = express.Router();
+var express     = require( 'express' );
+var router      = express.Router();
+const questions = [
+	{
+		id:      1,
+		text:    "Une question ?",
+		answers: [
+			{
+				text:  'answer 1',
+				value: true
+			},
+			{
+				text:  'answer2',
+				value: false
+			}
+		]
+	}
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-})
+router.get( '/', function ( req, res, next ) {
+	res.render( 'index', { title: 'Express' } );
+} )
 
 // Page de détails (e.g. liste des objets)
-.get( '/manager.html', function ( req, res ) {
-	res.render('manager.ejs', []);
-} )
-
-.get( '/:idQuestion/question.html', function ( req, res ) {
-    res.render('question.ejs', req.params);
-} )
-
-// Page de détails d'un objet
-.get( '/:idQuestion/resultat.html', function ( req, res ) {
-    res.render('resultat.ejs', req.params);
-} )
-
-.use( function ( req, res, next ) {
-	res.setHeader( 'Content-Type', 'text/plain; charset=utf-8' );
-	res.status( 404 ).send( 'Vous êtes sur une page inconnue.' );
-} );
+	.get( '/manager.html', function ( req, res ) {
+		res.render( 'manager.ejs', [] );
+	} )
+	
+	.get( '/:idQuestion/question.html', function ( req, res ) {
+		console.log( req.params.idQuestion );
+		
+		res.render( 'question.ejs', {
+			question: questions[ req.params.idQuestion ]
+		} );
+	} )
+	
+	// Page de détails d'un objet
+	.get( '/:idQuestion/resultat.html', function ( req, res ) {
+		res.render( 'resultat.ejs', req.params );
+	} )
+	
+	.use( function ( req, res, next ) {
+		res.setHeader( 'Content-Type', 'text/plain; charset=utf-8' );
+		res.status( 404 ).send( 'Vous êtes sur une page inconnue.' );
+	} );
 
 module.exports = router;
