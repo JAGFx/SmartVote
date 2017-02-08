4848d69433a8df8c643869a3f7ea9c9e38d8a7a9
JAGFx <contact@jagfx.fr>
2017-02-08 15:07:21 +0100

    Feature Data: Add Inserts SQL files

---
+++ data/smartvote_question.sql
@@ -0,0 +1,3 @@
+INSERT INTO smartvote.question (text, tags) VALUES ('Jolie question de sa mère !!', null);
+INSERT INTO smartvote.question (text, tags) VALUES ('Plop generated', null);
+INSERT INTO smartvote.question (text, tags) VALUES ('Yolo Yellow !!', null);
