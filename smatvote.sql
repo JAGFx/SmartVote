cf64ff564765be2cf62b5d4954ff5772a1a88da7
JAGFx <contact@jagfx.fr>
2017-02-08 14:38:01 +0100

    Fix DDL: Update DB schema

--- smatvote.sql
+++ smatvote.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 07, 2017 at 08:07 PM
--- Server version: 5.7.11
--- PHP Version: 5.6.19
+-- Generation Time: Feb 08, 2017 at 01:37 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,8 +17,25 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `smatvote`
+-- Database: `smartvote`
 --
+CREATE DATABASE IF NOT EXISTS `smartvote`
+  DEFAULT CHARACTER SET utf8
+  COLLATE utf8_general_ci;
+USE `smartvote`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answers`
+--
+
+CREATE TABLE `answers` (
+  `id`   INT(11)      NOT NULL,
+  `text` VARCHAR(255) NOT NULL
+)
+  ENGINE = MyISAM
+  DEFAULT CHARSET = utf8;
 
 -- --------------------------------------------------------
 
@@ -27,13 +44,34 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `question` (
-  `id` int(11) NOT NULL,
-  `text` text NOT NULL
+  `id`   int(11) NOT NULL,
+  `text` TEXT    NOT NULL,
+  `tags` JSON DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question_answer`
+--
+
+CREATE TABLE `question_answer` (
+  `question_id` INT(11)     NOT NULL,
+  `answer_id`   INT(11)     NOT NULL,
+  `value`       VARCHAR(50) NOT NULL
+)
+  ENGINE = MyISAM
+  DEFAULT CHARSET = utf8;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `answers`
+--
+ALTER TABLE `answers`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `question`
@@ -42,14 +80,28 @@ ALTER TABLE `question`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `question_answer`
+--
+ALTER TABLE `question_answer`
+  ADD KEY `question_answer_question_id_fk` (`question_id`),
+  ADD KEY `question_answer_answers_id_fk` (`answer_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `answers`
+--
+ALTER TABLE `answers`
+  MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 4;
+--
 -- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
