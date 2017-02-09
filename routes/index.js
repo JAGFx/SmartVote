6c6c84a146ccb029727f189b28c95cdd8455bdff
JAGFx <contact@jagfx.fr>
2017-02-09 10:02:00 +0100

    Feature Question: Add tags attr & methods

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express   = require( 'express' );
 var router    = express.Router();
-//var mysql     = require( '../components/mysql_dao' );
 var QuestionService = require( '../services/question.service.js' );
 
 var questions = [
@@ -24,7 +23,7 @@ var questions = [
 
 /* GET home page. */
 router.get( '/', function ( req, res ) {
-
+	
 	// INSERT ENTITY
 	/*mysql.insertEntity( 'question', {
 		id:   2,
@@ -32,7 +31,7 @@ router.get( '/', function ( req, res ) {
 	}, function ( err, res ) {
 		console.log( err, res );
 	} );*/
-
+	
 	// UPDATE
 	/*mysql.updateEntity( 'question', {
 		id:   2,
@@ -40,27 +39,27 @@ router.get( '/', function ( req, res ) {
 	}, function ( err, res ) {
 		console.log( err, res );
 	} );*/
-
+	
 	// DELETE
 	/*mysql.deleteEntity( 'question', 3, function ( err, res ) {
 		console.log( err, res );
 	} );*/
-
+	
 	// SELECT
 	/*var q = 'SELECT * FROM question';
 	mysql.find( q, [] , function ( err, res ) {
 		console.log( err, res );
 	} );*/
-
+	
 	// Example User QuestionService
-	/*QuestionService.findAll( function ( questions ) {
+	QuestionService.findAllByTags( [ 'SF3' ], function ( questions ) {
 		console.log( questions );
-
-		res.render( 'index.ejs', { title: questions.length } );
-	 } );*/
+		
+		res.render( 'index.ejs', { title: questions } );
+	} );
 	//res.render( 'index.ejs', { title: '' } );
-
-	res.redirect('/users/login');
+	
+	//res.redirect('/users/login');
 } )
 
 .get('/manager.html', function ( req, res ) {
