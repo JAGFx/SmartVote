17c73a12b6e56285807722a9f0d861f1539c1fcf
Humbert <moreaux.humbert@gmail.com>
2017-02-09 11:32:28 +0100

    Besoin de modifier le form et la fonction js associé afin d'emmetre la réponse

--- public/javascripts/question.js
+++ public/javascripts/question.js
@@ -9,7 +9,17 @@ var student = {
 
 $(function() {
     $('div.question').css('display', 'none');
+    $('input').attr('data-selected', 'false').click(switchSelectedValue(this));
 });
+
+function switchSelectedValue(element) {
+    var element = $(element);
+    if (element.data('selected') == 'true') {
+        element.data('selected', 'false');
+    } else {
+        element.data('selected', 'true');
+    }
+}
 
 socket.emit('newStudentConnection', student);
 
@@ -31,4 +41,13 @@ $('form').submit(function(e) {
 
 function displayQuestionById(questionId) {
     $('div#'+questionId).css('display', "block");
+}
+
+function confirmAnswer(evt) {
+    var questionButton = $(evt.currentTarget);
+    var questionId = questionButton.data('questionId');
+    console.log(questionId);
+    questionButton.parent().find("input[data-selected='true']");
+    console.log(questionButton.parent().find("input[data-selected='true']"));
+    console.log(questionButton.parent().find("input[data-selected='true']").val());
 }
