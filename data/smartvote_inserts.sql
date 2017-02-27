1cf52b8e82644fa2e68aa809d13b73349e93e2a4
JAGFx <contact@jagfx.fr>
2017-02-27 20:28:54 +0100

    Fix SQL Inserts: Add answer entries

--- data/smartvote_inserts.sql
+++ data/smartvote_inserts.sql
@@ -1,3 +1,4 @@
 INSERT INTO smartvote.answers VALUES (0, 'Oui');
 INSERT INTO smartvote.answers VALUES (1, 'Non');
-INSERT INTO smartvote.answers VALUES (2, 'Peut-être');
+INSERT INTO smartvote.answers VALUES (2, 'Aucun');
+INSERT INTO smartvote.answers VALUES (3, 'Les deux');
